9b23ea21945bdc14162772e6ea6fd167ef2ac911
Guraasees Singh <guraaseessingh389@gmail.com>
2025-07-30 21:02:34 +0530

    frontend Buttons

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,5 +1,7 @@
 
 import './App.css'
+import PlusIcon from './components/Icons/PlusIcon'
+import ShareIcon from './components/Icons/ShareIcon'
 import { Button } from './components/ui/Button'
 
 function App() {
@@ -7,12 +9,16 @@ function App() {
 
   return (
     <>
-    <Button variant ="primary" text="Continue" size='lg' startIcon={<>+</>} 
+    <Button variant ="primary" text="Continue" size='lg' startIcon={< PlusIcon size={props}  />} 
     onClick={()=>(alert("Button Clicked Paise daldo iske abb"))}></Button>  
-    <Button variant ="secondary" text="2ndue" size='sm' startIcon={<>-</>} 
+    <Button variant ="secondary" text="2ndue" size='sm' startIcon={< ShareIcon size="md"   />} 
     onClick={()=>(alert("Button Clicked Paise nikalo iske abb"))}></Button>  
     </>
   )
 }
 
 export default App
+//try to same sm of button come to icons size as well
+//declare from global for ts ignore removal 
+//complete backend apis
+
--- Frontend/src/components/ui/Button.tsx
+++ Frontend/src/components/ui/Button.tsx
@@ -10,23 +10,24 @@ interface ButtonProps {
 
 }
 const variantStyles = {
-    "primary":"bg-black-900  text-grey-200",
-    "secondary":"bg-white-900 text-grey-900"
+    "primary":"bg-black-900 hover:bg-black-500  text-grey-200",
+    "secondary":"bg-white-900 hover:text-black-900 text-grey-900"
 }
-const defaultStyles="rounded-lg  m-8 cursor-pointer border-solid"
+const defaultStyles="rounded-lg border-2 border-gray-300 hover:border-gray-700  m-8 cursor-pointer border-solid flex items-center"
 
 const sizeStyles={
-    "lg":"px-12 py-4",
-    "md":"px-4 py-2",
+    "lg":"px-12 py-4 text-lg",
+    "md":"px-4 py-2 text-md",
     "sm":"px-8 py-2"
 }
 //to do startIcon and endIcon 
 //use svg = scalable vector graphics doesn't fattna on zooming as it has path and eveything to know what's image unlike jpex jgeg and png which gets pixelated 
+
 export const Button=(props:ButtonProps)=>{
     
     return <div>
 <button className={`${variantStyles[props.variant]} ${defaultStyles} ${sizeStyles[props.size]} `} >
-    {props.text} 
+   {props.startIcon ? <div className="pr-2"> {props.startIcon}</div> : null} {props.text} {props.endIcon} 
 </button>
     </div>
 }
