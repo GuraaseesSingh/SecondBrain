8c118ea093980fd46176824aeef446e23ca7787f
Guraasees Singh <guraaseessingh389@gmail.com>
2025-08-06 12:42:10 +0530

    Updated charset of Hasher

--- Backend/src/linkgenerator.ts
+++ Backend/src/linkgenerator.ts
@@ -1,9 +1,11 @@
 export default function random(num: number) {
 let hash=""
-let from = "12838383883829103810"
+const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abs0192";
+
     for (let i=0; i < num; i++){
-        hash += from[Math.floor(Math.random()*from.length)] //math.random give b/w 0 to from.length floor gives decimal 
+        hash += charset[Math.floor(Math.random()*charset.length)] //math.random give b/w 0 to from.length floor gives decimal 
     }
     return hash;
     
 }
+//implement crypto security 
