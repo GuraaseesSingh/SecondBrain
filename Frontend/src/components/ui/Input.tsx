883e7dec919dc0bb8a749f7d96ae2e55ad3859a4
Guraasees Singh <guraaseessingh389@gmail.com>
2025-08-13 15:55:26 +0530

    Added Input Component

---
+++ Frontend/src/components/ui/Input.tsx
@@ -0,0 +1,24 @@
+
+export function Input({placeholder }) {
+  return (
+    <div className="m-2">
+      <input
+        type="text"
+        placeholder={placeholder}
+        className="
+          w-full px-3 py-2
+          rounded-md
+          bg-black-500
+          text-white
+          placeholder-gray-400
+          border border-gray-600
+          focus:border-purple-500
+          focus:ring-2 focus:ring-purple-500
+          outline-none
+          transition-all duration-200
+          font-mono
+        "
+      />
+    </div>
+  );
+}
